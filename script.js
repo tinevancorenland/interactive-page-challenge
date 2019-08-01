9fd4c7aedec841483623b532353d8e1e8a01c992
tinevancorenland <tinevancorenland@gmail.com>
2019-08-01 16:04:20 +0200

    add tab opening and closing functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Photoslider
+
 var i = 0;
 var images = [];
 var time = 3000;
@@ -37,4 +39,21 @@ function previousImg() {
     } else {
         i = 0
     }
+}
+
+// Tabs
+
+function openTab(evt, tabName) {
+    var tabContent = document.getElementsByClassName("infoContainer");
+    for (var i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    var tabLinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
