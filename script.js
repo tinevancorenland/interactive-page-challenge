86cbabb7df45009d7c56bed4c70cc209649e7a93
tinevancorenland <tinevancorenland@gmail.com>
2019-08-01 11:08:42 +0200

    add javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+var i = 0;
+var images = document.getElementsByTagName("img");
+console.log(images);
+var time = 3000;
+
+images[0] = "./img/AbovePhoto.jpeg";
+images[1] = "./img/Astronaut.jpeg";
+images[2] = "./img/SpaceStationOverBelgium.jpeg"
+
