2e6cbfb4c74e84d7e11d330e21e71a0bcf8cb561
tinevancorenland <tinevancorenland@gmail.com>
2019-08-01 11:28:50 +0200

    add change img function every 3 sec

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var i = 0;
-var images = document.getElementsByTagName("img");
-console.log(images);
+var images = [];
 var time = 3000;
 
 images[0] = "./img/AbovePhoto.jpeg";
 images[1] = "./img/Astronaut.jpeg";
 images[2] = "./img/SpaceStationOverBelgium.jpeg"
 
+function changeImg() {
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+}
